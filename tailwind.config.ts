5f97e4e145f9e9a04e938402f57557e6cbd18431
Daniel Dihardja <daniel@dihardja.de>
2024-04-22 10:58:58 +0200

    fix css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 import withMT from "@material-tailwind/react/utils/withMT";
 
-/** @type {import('tailwindcss').Config} */
 export default withMT({
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
